cedff76c09371522f0dfb0767671d5339597697f
Periapsises <94928308+Periapsises@users.noreply.github.com>
2025-10-17 13:10:08 -0400

    Improve workspace and project creation

--- premake5.lua
+++ premake5.lua
@@ -1,52 +1,117 @@
-local realmPrefixes = {
-    Client = "gmcl_",
-    Server = "gmsv_",
+ModuleRealm = {
+    Client = 1,
+    Server = 2,
+    Both = 3,
 }
 
-local architecture
+ModuleArchitecture = {
+    x86 = 1,
+    x64 = 2,
+    Both = 3,
+}
 
-function SetupModule(settings)
-    if not settings then settings = {} end
+local realms = {
+    [ModuleRealm.Client] = {"Client"},
+    [ModuleRealm.Server] = {"Server"},
+    [ModuleRealm.Both] = {"Client", "Server"},
+}
 
-    local realms = settings.realms or { "Client", "Server" }
-    local architectures = settings.architectures or { "x86", "x64" }
+local architectures = {
+    [ModuleArchitecture.x86] = {"x86"},
+    [ModuleArchitecture.x64] = {"x64"},
+    [ModuleArchitecture.Both] = {"x86", "x64"},
+}
+
+function CreateModuleWorkspace(workspaceName, moduleRealm, moduleArchitecture)
+    workspace(workspaceName)
+
+    language "C++"
+    warnings "Extra"
+    flags { "MultiProcessorCompile", "ShadowedVariables", "UndefinedIdentifiers" }
+    characterset "MBCS"
+    intrinsics "On"
+    inlining "Auto"
+    rtti "On"
+    vectorextensions "SSE2"
+
+    if moduleArchitecture == ModuleArchitecture.x86 then
+        platforms "x86"
+    elseif moduleArchitecture == ModuleArchitecture.x64 then
+        platforms "x86_64"
+    elseif moduleArchitecture == ModuleArchitecture.Both then
+        platforms { "x86", "x86_64" }
+    end
+
+    filter "platforms:x86"
+        architecture "x86"
+    filter "platforms:x86_64"
+        architecture "x86_64"
+    filter {}
 
     local configs = {}
 
-    for _, realm in ipairs(realms) do
-        if not realmPrefixes[realm] then
-            error("Invalid realm: " .. realm .. " in settings table. Only 'Client' and 'Server' are allowed.")
-        end
-
-        for _, architecture in ipairs(architectures) do
-            table.insert(configs, realm .. "_" .. architecture .. "_Debug")
-            table.insert(configs, realm .. "_" .. architecture .. "_Release")
-        end
+    for _, realm in ipairs(realms[moduleRealm]) do
+        table.insert(configs, realm .. " Debug")
+        table.insert(configs, realm .. " Release")
     end
 
     configurations(configs)
 
-    for _, realm in ipairs(realms) do
-        for _, architecture in ipairs(architectures) do
-            local prefix = realmPrefixes[realm]
-            local suffix
+    filter "configurations:*Release*"
+        linktimeoptimization "On"
+        optimize "Full"
+        symbols "Off"
+        defines { "NDEBUG" }
+        runtime "Release"
+    
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        defines {
+            "_CRT_NONSTDC_NO_WARNINGS",
+            "_CRT_SECURE_NO_WARNINGS",
+            "STRICT",
+        }
+    
+    filter "system:linux"
+        cppdialect "GNU++17"
+        pic "On"
+        staticruntime "On"
+        linkoptions "-Wl,--no-undefined"
+    
+    filter {}
 
-            filter "system:windows"
-                suffix = architecture == "x86" and "_win32" or "_win64"
-            filter "system:linux"
-                suffix = architecture == "x86" and "_linux32" or "_linux"
+    strictaliasing "Level3"
+end
 
-            filter("configurations:*" .. realm .. "*" .. architecture .. "*")
+function CreateModuleProject(projectName)
+    project(projectName)
+    language "C++"
+    kind "SharedLib"
+    targetextension(".dll")
 
-            targetprefix(prefix)
-            targetsuffix(suffix)
-            targetextension(".dll")
-            
-            if realm == "Client" then
-                defines { "GMOD_MODULE_CLIENT" }
-            else
-                defines { "GMOD_MODULE_SERVER" }
-            end
+    local suffix
+
+    filter "system:windows"
+        suffix = "_win"
+    filter "system:linux"
+        suffix = "_linux"
+
+    filter "configurations:*Client*"
+        defines { "GMOD_MODULE_CLIENT" }
+        targetprefix("gmcl_")
+    filter "configurations:*Server*"
+        defines { "GMOD_MODULE_SERVER" }
+        targetprefix("gmsv_")
+    
+    filter "configurations:*x86*"
+        defines { "GMOD_MODULE_X86" }
+        if suffix == "_win" then
+            suffix = suffix .. "32"
         end
-    end
+
+    filter "configurations:*x64*"
+        defines { "GMOD_MODULE_X64" }
+        suffix = suffix .. "64"
+    filter {}
 end
